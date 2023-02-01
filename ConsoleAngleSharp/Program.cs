c20a7484bcf83b0f1040eef2d4459293d37c7393
yuguangjiang <yuguangjiang@liontravel.com>
2023-02-01 11:46:06 +0800

    建立Browser的配置

--- ConsoleAngleSharp/Program.cs
+++ ConsoleAngleSharp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AngleSharp;
+
+//建立Browser的配置
+var config = AngleSharp.Configuration.Default.WithDefaultLoader();
+
+//根據配置建立出Browser
+var browser = BrowsingContext.New(config);
+
 Console.WriteLine("Hello, World!");
