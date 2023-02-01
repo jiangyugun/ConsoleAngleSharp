a73464f2b2fbec4b6ee6ea7df095f42cac920c66
yuguangjiang <yuguangjiang@liontravel.com>
2023-02-01 15:44:34 +0800

    簡易測試Console讀出特定網站每個標題

--- ConsoleAngleSharp/Program.cs
+++ ConsoleAngleSharp/Program.cs
@@ -2,18 +2,20 @@
 using AngleSharp.Dom;
 using LINQPad;
 
-// 這邊為了方便處理也順便把 `Main` 改成非同步的版本
-async Task Main()
+var config = AngleSharp.Configuration.Default.WithDefaultLoader();
+var browser = BrowsingContext.New(config);
+
+// 這邊用的型別是 AngleSharp 提供的 AngleSharp.Dom.Url
+var url = new Url("https://dannyliu.me/");
+
+// 使用 OpenAsync 來打開網頁抓回內容
+var document = browser.OpenAsync(url).Result;
+
+//QuerySelector(".entry-content")找出class="entry-content"的所有元素
+var contents = document.QuerySelectorAll(".entry-content");
+
+foreach (var c in contents)
 {
-	var config = AngleSharp.Configuration.Default.WithDefaultLoader();
-	var browser = BrowsingContext.New(config);
-
-	// 這邊用的型別是 AngleSharp 提供的 AngleSharp.Dom.Url
-	var url = new Url("https://www.ptt.cc/bbs/Beauty/index.html");
-
-	// 使用 OpenAsync 來打開網頁抓回內容
-	var document = await browser.OpenAsync(url);
-	document.Dump();
+    //取得每個元素的TextContent
+    Console.WriteLine(c.TextContent);
 }
-
-Console.WriteLine("Hello, World!");
