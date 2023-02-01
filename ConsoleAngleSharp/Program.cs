db3bca6a548fe34e2c758a6495966f72ebdc68db
yuguangjiang <yuguangjiang@liontravel.com>
2023-02-01 11:54:26 +0800

    安裝LINQPad

--- ConsoleAngleSharp/Program.cs
+++ ConsoleAngleSharp/Program.cs
@@ -1,9 +1,19 @@
 ﻿using AngleSharp;
+using AngleSharp.Dom;
+using LINQPad;
 
-//建立Browser的配置
-var config = AngleSharp.Configuration.Default.WithDefaultLoader();
+// 這邊為了方便處理也順便把 `Main` 改成非同步的版本
+async Task Main()
+{
+	var config = AngleSharp.Configuration.Default.WithDefaultLoader();
+	var browser = BrowsingContext.New(config);
 
-//根據配置建立出Browser
-var browser = BrowsingContext.New(config);
+	// 這邊用的型別是 AngleSharp 提供的 AngleSharp.Dom.Url
+	var url = new Url("https://www.ptt.cc/bbs/Beauty/index.html");
+
+	// 使用 OpenAsync 來打開網頁抓回內容
+	var document = await browser.OpenAsync(url);
+	document.Dump();
+}
 
 Console.WriteLine("Hello, World!");
